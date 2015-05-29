2b4bd18f9451fb5d8d8e656c7568d443c1a7f239
sammcgrail <sammcgrail@gmail.com>
2015-05-29 10:21:23 -0400

    got stuck converting, placeholder

--- server.rb
+++ server.rb
@@ -7,22 +7,34 @@ require "sinatra"
 
 def db_connection
   begin
-    connection = PG.connect(dbname: "nba")
+    connection = PG.connect(dbname: "grocery_list")
     yield(connection)
   ensure
     connection.close
   end
 end
 
-nba = CSV.readlines("grocery_list.txt", headers: true)
+nba = CSV.readlines("grocery_list.csv", headers: true)
 
+def groceries_main
+inserter =
+        "INSERT INTO grocery_list (
+          item,
+          type,
+          amount
+        ) VALUES ($1, $2, $3)"
+        groceries = CSV.readlines("grocery_list.csv", headers: true)
+  groceries.each do |groceries|
+    item = groceries["item"]
+    type = groceries ["type"]
+    amount = groceries["amount"]
+    elements = [item,type,amount]
 
-
-
-
-
-
-
+    groceries_main = db_connection do |conn|
+      conn.exec_params(inserter, elements)
+    end
+  end
+end
 
 
 get '/' do
@@ -30,20 +42,17 @@ get '/' do
 end
 
 get '/groceries' do
-  @items = File.readlines('grocery_list.txt')
+  @items = CSV.readlines("grocery_list.csv", headers: true)
   erb :index
 end
 
 post '/groceries' do
-
-
-
   if params['item'] == ""     # how to catch blank name in form
     redirect '/groceries'
   end
   item = params['item']
 
-  File.open('grocery_list.txt', 'a') do |file|
+  CSV.open('grocery_list.csv', 'a') do |file|
     file.puts(item)
   end
 
